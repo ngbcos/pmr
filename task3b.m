f6b43609a3b3b7232af197df364e2a2227550292
Georgi Tinchev <s1549117@ciociosan.inf.ed.ac.uk>
2016-03-07 20:27:14 +0000

    Task 1 and 2 described in the report.

--- task3b.m
+++ task3b.m
@@ -10,13 +10,13 @@ function [] = task3b()
     figure(1);
     hold on;
     title('Diagonal covariance matrix');
-    plot(x, normpdf(x, approx_mean(lee_sedol_id), approx_covar(lee_sedol_id)),'-r');
-    plot(x, normpdf(x, approx_mean(fan_hui_id), approx_covar(fan_hui_id)),'--g');
-    plot(x, normpdf(x, approx_mean(alpha_go_id), approx_covar(alpha_go_id)),'-.b');
-    ylim([0 1.6]);
+    plot(x, normpdf(x, approx_mean(lee_sedol_id), approx_covar(lee_sedol_id)^0.5),'-r');
+    plot(x, normpdf(x, approx_mean(fan_hui_id), approx_covar(fan_hui_id)^0.5),'--g');
+    plot(x, normpdf(x, approx_mean(alpha_go_id), approx_covar(alpha_go_id)^0.5),'-.b');
+    ylim([0 9]);
     legend('Lee Sedol','Fan Hui', 'AlphaGo', 'Location', 'northwest');
-    xlabel('Position along line s');
-    ylabel('Value of factor at s=s(x^{(k)T}x^{(k)})^{-1}x^{(k)}');
+    xlabel('Skill');
+    ylabel('P(Skill)');
     hold off;
     
     load('go_player_skill_model/full_covar.mat');
@@ -24,13 +24,13 @@ function [] = task3b()
     figure(2);
     hold on;
     title('Full covariance matrix');
-    plot(x, normpdf(x, approx_mean(lee_sedol_id), approx_covar(lee_sedol_id,lee_sedol_id)),'-r');
-    plot(x, normpdf(x, approx_mean(fan_hui_id), approx_covar(fan_hui_id,fan_hui_id)),'--g');
-    plot(x, normpdf(x, approx_mean(alpha_go_id), approx_covar(alpha_go_id,alpha_go_id)),'-.b');
-    ylim([0 1.6]);
+    plot(x, normpdf(x, approx_mean(lee_sedol_id), approx_covar(lee_sedol_id,lee_sedol_id)^0.5),'-r');
+    plot(x, normpdf(x, approx_mean(fan_hui_id), approx_covar(fan_hui_id,fan_hui_id)^0.5),'--g');
+    plot(x, normpdf(x, approx_mean(alpha_go_id), approx_covar(alpha_go_id,alpha_go_id)^0.5),'-.b');
+    ylim([0 9]);
     legend('Lee Sedol','Fan Hui', 'AlphaGo', 'Location', 'northwest');
-    xlabel('Position along line s');
-    ylabel('Value of factor at s=s(x^{(k)T}x^{(k)})^{-1}x^{(k)}');
+    xlabel('Skill');
+    ylabel('P(Skill)');
     hold off;
     drawnow;
 end
